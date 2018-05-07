701233927beb1522588eab5f892026fe8b8539ed
Dominic Fung <fung_dominic@hotmail.com>
2018-05-06 20:50:48 -0400

    nodis reference added instead of nex, gitignor avm debug.json

--- ico_template.py
+++ ico_template.py
@@ -8,10 +8,10 @@ Email: tom@neonexchange.org
 Date: Dec 11 2017
 
 """
-from nex.txio import get_asset_attachments
-from nex.token import *
-from nex.crowdsale import *
-from nex.nep5 import *
+from nodis.txio import get_asset_attachments
+from nodis.token import *
+from nodis.crowdsale import *
+from nodis.nep5 import *
 from boa.interop.Neo.Runtime import GetTrigger, CheckWitness
 from boa.interop.Neo.TriggerType import Application, Verification
 from boa.interop.Neo.Storage import *
